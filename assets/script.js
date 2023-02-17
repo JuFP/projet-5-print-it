e2555dfdf29529a629be9727b03bd384e0249ce6
Justine Faria Pereira <justine.fariapereira@yahoo.com>
2023-02-17 10:59:20 +0100

    intégration du html dynamique des bullet points

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+let i = 0;
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -16,19 +17,51 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-//CLICK EVENTS//
-const arrowLeft = document.querySelector(".arrow_left");
-const arrowRight = document.querySelector(".arrow_right");
 
-arrowLeft.addEventListener('click', () => {
-	alert("Hello world");
+const prev = document.querySelector(".arrow_left");
+const next = document.querySelector(".arrow_right");
+const selected = document.getElementsByClassName("dot_selected");
+
+let carousel = document.getElementById("banner");
+let carouselImage = document.getElementsByClassName("banner-img");
+let carouselText = document.querySelector(".banner-p");
+
+function addBullets() {
+	const divDots = document.querySelector(".dots");   
+	for (
+		let i =0;
+		i <slides.length; 
+		i++ ) {        //tant que l'indice est inférieur au nb d'éléments du tableau, on ajoute//
+		const dot = document.createElement("span");
+		dot.classList.add("dot");    //ajout des span de class dot//
+		if (
+			i === 0
+			) {									
+			dot.classList.add("dot_selected"); //ajout de la class dot_selected au premier élément du tableau(indice0)//
+		}
+		divDots.appendChild(dot);     //dans la div de class dots//
+	}
+};
+addBullets(); // exécution du html dynamique des bullets points//
+
+function addText() {
+	carouselText.innerHTML = "Impressions tous formats <span>en boutique et en ligne</span>";
+}
+addText(); //exécution du html dynamique du texte//
+
+//CLICK EVENTS//
+prev.addEventListener('click', () => {	
+		
 });
-arrowRight.addEventListener('click', () => {
-	alert("Hello world");
+next.addEventListener('click', () => {
+	selected[0].classList.remove("dot_selected");
+	selected[1].classList.add("dot_selected");
+	
 });
-//défilements//
-const dots = document.querySelector(".dots");
-const dot = document.querySelector(".dot");
+
+
+
+
 
 
 
