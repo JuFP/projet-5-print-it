40e846e631c35462224bcc37116cca5cf1c40c61
Justine Faria Pereira <justine.fariapereira@yahoo.com>
2023-02-23 11:41:44 +0100

    ajout des bullet points

--- assets/script.js
+++ assets/script.js
@@ -19,6 +19,7 @@ const slides = [
 ]
 const prev = document.querySelector(".arrow_left");
 const next = document.querySelector(".arrow_right");
+const urlImage = "./assets/images/slideshow/";
 
 let carouselImage = document.querySelector(".banner-img");
 let carouselText = document.querySelector("p");
@@ -26,26 +27,44 @@ let bullets = document.getElementsByClassName("dot");
 let selected = document.querySelector("dot_selected");
 
 function turnRight() {
+	bullets[i].classList.remove ("dot_selected"); 
 	i++; //incrémentation de 1 du compteur//
 	if (
-		i === slides.length) { //si compteur égal au nb d'objets du tableau//
+		i >= slides.length) { //si compteur égal au nb d'objets du tableau//
 	  	i = 0;     //<-----compteur réinitialisé à 0//
 	}
 	carouselText.innerHTML = slides[i].tagLine; //changement du texte en fonction de l'indice//
-	carouselImage.src = "./assets/images/slideshow/" + slides[i].image; //changement de la source image en fonction de l'indice//
+	carouselImage.src = urlImage + slides[i].image; //changement de la source image en fonction de l'indice//
+	bullets[i].classList.add ("dot_selected");
 };
 function turnLeft() {
+	bullets[i].classList.remove ("dot_selected"); 
 	i --; //décrémentation de 1 du compteur//
 	if (
 		i < 0) { //si indice compteur inférieur à 0 (atteinte de la première image)//
 		i = slides.length -1; //réinitialisation du compteur à la dernière image du tableau//
 	} 
 	carouselText.innerHTML = slides[i].tagLine;
-	carouselImage.src ="./assets/images/slideshow/"+slides[i].image;
+	carouselImage.src = urlImage + slides[i].image;
+	bullets[i].classList.add ("dot_selected");
 };
-
-
-
+function addBullets() {
+	const divDots = document.querySelector(".dots");   
+	for (
+		let i =0;
+		i <slides.length; 
+		i++ ) {        //tant que l'indice est inférieur au nb d'éléments du tableau, on ajoute//
+		const dot = document.createElement("span");
+		dot.classList.add("dot");    //ajout des span de class dot//
+		if (
+			i === 0
+			) {									
+			dot.classList.add("dot_selected"); //ajout de la class dot_selected au premier élément du tableau(indice0)//
+		}
+		divDots.appendChild(dot);     //dans la div de class dots//
+	}
+};
+addBullets();
 //CLICK EVENTS//
 prev.addEventListener('click', () => {	
 	turnLeft(); //appel de la fonction//
